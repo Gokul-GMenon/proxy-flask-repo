138ef4ae4bbf6f8f7aa6c48a62c51255fd05affd
Gokul G Menon <gokulgovindmenon@gmail.com>
2025-04-19 21:18:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# On the external server (e.g. Flask API):
+@app.route("/proxy_image", methods=['GET']))
+def proxy():
+    image_url = request.args.get("url")
+    img = requests.get(image_url).content
+    return Response(img, mimetype=img.headers.get("Content-Type", "image/png")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
